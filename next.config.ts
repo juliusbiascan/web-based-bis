62662d1551abfb3a055c3cece13b66a94f4b2a81
Julius Biascan <juliusbiascan.bscs.pass@gmail.com>
2025-10-26 02:47:36 +0800

    fix: update Prisma client output path and configuration for consistency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@prisma/client": "./lib/generated/prisma",
+      },
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@prisma/client": path.resolve(__dirname, "lib/generated/prisma"),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;

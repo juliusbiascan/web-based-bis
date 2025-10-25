3f936727612afeb4ed09710b472a277c3fd1f4aa
Julius Biascan <juliusbiascan.bscs.pass@gmail.com>
2025-10-26 02:36:10 +0800

    fix: remove Prisma client aliasing from configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        "@prisma/client": "./lib/generated/prisma",
-      },
-    },
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@prisma/client": path.resolve(__dirname, "lib/generated/prisma"),
-    };
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
